8b7e2d301fa1bfd53fff315d290d69b431147950
Luis Martinell Andreu <luis@catchoom.com>
2014-01-13 16:13:52 +0100

    Created configuration to compile AFNetworking as an universal framework.

---
+++ Example/AFNetworkingTests/AFNetworkingTests.m
@@ -0,0 +1,34 @@
+//
+//  AFNetworkingTests.m
+//  AFNetworkingTests
+//
+//  Created by Luis Martinell Andreu on 1/13/14.
+//  Copyright (c) 2014 Gowalla. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface AFNetworkingTests : XCTestCase
+
+@end
+
+@implementation AFNetworkingTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
